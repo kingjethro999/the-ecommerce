9c3d23ffe500b442747d6a23c822d3fc2a0a7d56
King Jethro <jethrojerrybj@gmail.com>
2025-09-03 11:21:25 +0100

    removed Clerk authremoved Clerk auth

--- app/(stripe)/layout.tsx
+++ app/(stripe)/layout.tsx
@@ -1,18 +1,10 @@
 // removed Clerk auth
-import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
-export default async function CheckoutLayout({
+export default function CheckoutLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  const { userId } = await auth();
-
-  if (!userId) {
-    // If no user is signed in, redirect to the sign-in page
-    redirect("/sign-in");
-  }
-
   return <div>{children}</div>;
 }
